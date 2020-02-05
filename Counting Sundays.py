0d5e28f9500ad7bb1e1cfdda1fe48948bf2a8527
algebra-det <algebra-det@users.noreply.github.com>
2020-02-06 00:59:48 +0530

    Count Number of Sundays between years

--- Counting Sundays.py
+++ Counting Sundays.py
@@ -35,3 +35,32 @@ for year in range(1901,2001):
 
 print(sunday_count)
 # print 171
+
+# Another way of doing the same
+def hms():
+	months=[31,28,31,30,31,30,31,31,30,31,30,31]
+	months2=[31,29,31,30,31,30,31,31,30,31,30,31]
+	day=2
+	count=0
+	for i in range (1901,2001):
+		for j in range (0,12):
+			if i%4!=0:
+				day+=months[j]%7
+			else:
+				day+=months2[j]%7
+			if day%7==0:
+				count+=1
+	return count
+
+number = hms()
+print(number)
+
+#Or
+
+import datetime
+count = 0
+for y in range(1901,2001):
+    for m in range(1,13):
+        if datetime.datetime(y,m,1).weekday() == 6:
+            count += 1
+print(count)
